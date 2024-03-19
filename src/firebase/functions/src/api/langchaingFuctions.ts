99fc4abcc04d1f413b45b66b7b33dcf3aace6aee
Alexander Krustev <krustev.alexandr@elveder.com>
2024-03-19 19:21:34 +0200

    Fixes

--- src/firebase/functions/src/api/langchaingFuctions.ts
+++ src/firebase/functions/src/api/langchaingFuctions.ts
@@ -11,16 +11,38 @@ const MISTRAL_API_KEY = '5XikMZrBKsmEv218407LqvLL3FapuuDg';
 const QDRANT_URL = 'https://fad86663-a2dc-4852-91da-a3cbfa76f130.us-east4-0.gcp.cloud.qdrant.io';
 const QDRANT_API_KEY = 'M2s39zNtibfQ9G4JP2pHKyvqsLwJlR8P7k19tw2TKcVTZg7xJ3lYzA';
 
+interface CarDataModel {
+    carMaker: string | null,
+    model: string | null,
+    year: string | null
+}
+
 export const prompt = onRequest(async (request, response) => {
     const data = parsePostData(request);
     logger.log(data);
 
+    const carData = await _getCarData(data.input);
+
+    if(carData == undefined) {
+        response.send('It seems there is some issue on our end, and couldn\'t  process your request. Please try again.');
+        return;
+    }
+
+    if(!carData.carMaker || !carData.model || !carData.year) {
+        response.send('We couldn\'t process your request because there might be an issue with the car maker, model, or year information provided. Please ensure you have mentioned all three and try again.');
+        return;
+    }
+
+    response.send(carData);
+});
+
+async function _getCarData(input: string): Promise<CarDataModel | undefined>{
     const chatModel = new ChatMistralAI({
         apiKey: MISTRAL_API_KEY,
         modelName: 'mistral-small-latest',
     });
 
-    const prompt =ChatPromptTemplate.fromTemplate(`
+    const prompt = ChatPromptTemplate.fromTemplate(`
         What car is the user talking about?
         Distinguish the CAR MAKER, the MODEL and the YEAR from the User Input.
 
@@ -34,55 +56,55 @@ export const prompt = onRequest(async (request, response) => {
         }}
         ///
         
-        If cannot distinguish the CAR MAKER, the MODEL and the YEAR, or if the input is not related to cars, don't gues; instead use "Unknown".
+        If cannot distinguish the CAR MAKER, the MODEL and the YEAR, or if the input is not related to cars, don't gues; instead use null.
 
         User Input: I'm seriously considering buying a 2020 Tesla Model S for its cutting-edge technology features.
         {{
             "carMaker": "Tesla",
-            "model: "Model S",
+            "model": "Model S",
             "year": "2020"
         }}
 
         User Input: I've always dreamed of owning a piece of American history, so I'm on the lookout to buy a 1964 Ford Mustang.
         {{
             "carMaker": "Ford",
-            "model: "Mustang",
+            "model": "Mustang",
             "year": "1964"
         }}
 
         User Input: After reading numerous reviews about its dependability and stylish design, I've decided I want to buy a 2018 Toyota Camry.
         {{
             "carMaker": "Toyota",
-            "model: "Camry",
+            "model": "Camry",
             "year": "2018"
         }}
 
         User Input: I want to buy some BMW.
         {{
             "carMaker": "BMW",
-            "model: "Unknown",
-            "year": "Unknown"
+            "model": null,
+            "year": null
         }}
 
         User Input: I am thinking of getting some Mercedes-Benz S-class. But I am still researching for the year.
         {{
             "carMaker": "Mercedes-Benz",
-            "model: "S-Class",
-            "year": "Unknown"
+            "model": "S-Class",
+            "year": null
         }}
 
         User Input: I really like Porche from 2018.
         {{
             "carMaker": "Porche",
-            "model: "Unknown",
+            "model": null,
             "year": "2018"
         }}
 
         User Input: What would the weather be like in May?
         {{
-            "carMaker": "Unknown",
-            "model: "Unknown",
-            "year": "Unknown"
+            "carMaker": null,
+            "model": null,
+            "year": null
         }}
 
         User Input: {input}
@@ -93,11 +115,21 @@ export const prompt = onRequest(async (request, response) => {
     const chain = prompt.pipe(chatModel).pipe(outputParser);
 
     const result = await chain.invoke({
-        input: data.input
+        input: input
     });
 
-    response.send(result);
-});
+    console.log(result);
+
+    try {
+        const carData = JSON.parse(result);
+
+        return carData;
+    } catch (e: any) {
+        console.error(e);
+        
+        return undefined;
+    }
+}
 
 export const embeddings = onRequest(async (request, response) => {
     const data = parsePostData(request);
