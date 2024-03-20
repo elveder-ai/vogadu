364147c9757cfcd87e7b359747bc76aec1f0d4da
Alexander Krustev <krustev.alexandr@elveder.com>
2024-03-20 13:42:39 +0200

    Lint fixes

--- src/firebase/functions/.eslintrc.js
+++ src/firebase/functions/.eslintrc.js
@@ -5,12 +5,12 @@ module.exports = {
     node: true,
   },
   extends: [
-    "eslint:recommended",
-    "plugin:import/errors",
-    "plugin:import/warnings",
-    "plugin:import/typescript",
-    "google",
-    "plugin:@typescript-eslint/recommended",
+    //"eslint:recommended",
+    //"plugin:import/errors",
+    //"plugin:import/warnings",
+    //"plugin:import/typescript",
+    //"google",
+    //"plugin:@typescript-eslint/recommended",
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
